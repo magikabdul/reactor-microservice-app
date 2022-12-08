a9aa65d1e31b1b2f67f5f6894b6613c223ef6006
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-12-08 23:01:58 +0100

    added gender-service

---
+++ gender-service/src/main/java/cloud/cholewa/gender_service/GenderServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.gender_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenderServiceApplication.class, args);
+    }
+
+}
---
+++ gender-service/src/test/java/cloud/cholewa/gender_service/GenderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.gender_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
